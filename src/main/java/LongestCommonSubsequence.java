1f06579562957de07fe1ac4187b3ca19cbc5477f
DW <924004+daw42@users.noreply.github.com>
2023-11-17 08:35:19 -0800

    Initial import PA5

---
+++ src/main/java/LongestCommonSubsequence.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class LongestCommonSubsequence {
+
+    // The list of Strings, all length k each containing a permutation of the first k letters of the alphabet
+    private final List<String> strings;
+
+    // The number of characters in each string in strings
+    private final int k;
+
+    public LongestCommonSubsequence( List<String> strings, int k ) {
+        this.strings = strings;
+        this.k = k;
+    }
+
+    public int solve( ) {
+
+        // TODO: Implement your solution here.
+
+        return 0;
+    }
+}
